818fc4571de828a58a1d66c3c769b3a0730e3573
kytsaaa6 <kytsaaa6@naver.com>
2019-01-04 05:59:38 -0800

    Added views to create/edit blog post inside the site.

--- board/urls.py
+++ board/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<pk>', views.post_detail, name='post_detail'),
 ]
